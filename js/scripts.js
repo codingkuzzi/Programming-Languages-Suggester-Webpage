e0aa50890fe80ee2e2a5a0c21cd3548d292a9b64
Anna Kuznetsova <aakuznetsova@gmail.com>
2017-06-02 20:14:05 -0700

    add functions to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,47 @@
-$(function() {
-  $("#quiz").show();
-  $("form#quiz").submit(function(event) {
-    event.preventDefault();
-    var childhood = $("input:radio[name=childhood]:checked").val();
-    if (childhood === "drawing") {
-      $(".containerSurvey").empty();
-      $("#result1").show();
-    } else if (childhood === "lego") {
-      $(".containerSurvey").empty();
-      $("#result2").show();
-    } else if (childhood === "chess") {
-      $(".containerSurvey").empty();
-      $("#result3").show();
-    }
-    $("#retake").show();
+function initSurvey() {
+  $(".containerResult").hide();
+  $("input:radio:checked").prop('checked', false);
+  $(".containerSurvey").show();
+}
 
+function showResult(resultIndex) {
+  $(".containerSurvey").hide();
+  $("#result1").hide();
+  $("#result2").hide();
+  $("#result3").hide();
+  $(#result + resultIndex).show();
+  $(".containerResult").show();
+}
+
+function calculateResult() {
+  var childhood = $("input:radio[name=childhood]:checked").val();
+  switch (childhood) {
+    case "drawing":
+      return 1;
+    case "lego":
+      return 2;
+    case "chess":
+      return 3;
+        default:
+        return 0;
+  }
+}
+
+$(function() {
+  initSurvey();
+  $("form#quiz").submit(function (event) {
+    event.preventDefault();
+    var resultIndex = calculateResult();
+    if (resultIndex > 0) {
+      showResult(resultIndex);
+
+    }
   });
+
   $("#retake").click(function() {
-    $(".containerSurvey").empty();
-    $("#quiz").show();
+    initSurvey();
+
+
+
   });
 });
